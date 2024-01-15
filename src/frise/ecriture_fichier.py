748554125817bb9a0f12c443b55dcd9fa0784f8c
Romeo <romeo.ramostanghe@gmail.com>
2024-01-15 14:33:35 +0100

    ecriture fichier

---
+++ src/frise/ecriture_fichier.py
@@ -0,0 +1,13 @@
+"""
+Projet Episcope
+
+Auteur : Roméo RAMOS--TANGHE
+
+Ce fichier contient des fonctions pour créer et éditer les fichiers '.txt' de sortie avec les symptomes
+"""
+
+def EcrireSymptome(symptome, fichier) :
+
+
+
+def EcrireListeSymptome() :
