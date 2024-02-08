dc52375df3b1995857419a694f5774bd07528be1
Romeo <romeo.ramostanghe@gmail.com>
2024-02-08 09:43:33 +0100

    save txt operationnel

--- src/frise/save.py
+++ src/frise/save.py
@@ -6,6 +6,7 @@ Auteur : Roméo RAMOS--TANGHE
 Ce fichier contient des fonctions de base pour sauvegarder les fichiers '.txt' et frise de sortie
 """
 from tkinter import filedialog
+from tkinter import messagebox
 import customtkinter as CTK
 import ecriture_fichier as EF
 
@@ -15,7 +16,7 @@ class save :
     """
     def __init__(self, Liste_symptomes = []):
         self.symptomes = Liste_symptomes
-    
+        self.closewd = 0
 
     def save(self):
         """
@@ -23,64 +24,75 @@ class save :
         """
         # récuperer le nom et emplacement des fichiers
         filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Fichiers texte", "*.txt"), ("Tous les fichiers", "*.*")])
-        
-        # recuperer les métadatas
-        Meta_wd = metadata()
+        try :
+            # ecrire les métadatas
+            Meta_wd = metadata(filename)
 
-        if Meta_wd.closewd == 1 :
-            ListeMeta = Meta_wd.liste
-            EF.EcrireMetaData(ListeMeta, filename)
             EF.EcrireListeSymptome(self.symptomes, filename)
+            print("ecriture")
 
+        except FileNotFoundError:
             Meta_wd.destroy()
+            Meta_wd.update()
+            messagebox.showinfo('Episcope Error', 'No file selected\nCannot save properly')
+
+
+
         
         
-
-
-
-
 class metadata(CTK.CTkToplevel) :
     """
     fenetre toplevel pour saisir les metadonnées
     """
-    def __init__(self) :
+    def __init__(self,filename) :
         super().__init__()
 
         self.liste = []
-        self.closewd = 0
+        self.filename = filename
+
 
         self.title("Metadata")
-        self.geometry("400x150")
-        self.grid_columnconfigure(0, weight=1)
+        self.geometry("400x200")
+        self.grid_columnconfigure(3, weight=1)
 
         self.meta_label = CTK.CTkLabel(self, text="enter MetaData")
-        self.meta_label.grid(row=0, column=0, padx=5, pady=5)
+        self.meta_label.grid(row=0, column=1, padx=5, pady=5)
 
         # metadatas
         self.patient_entry = CTK.CTkEntry(self, placeholder_text="Patient name")
-        self.patient_entry.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+        self.patient_entry.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
+        self.patient_label = CTK.CTkLabel(self, text="Patient's name : ")
+        self.patient_label.grid(row=1, column=0, padx=5, pady=5)
+
         self.doctor_entry = CTK.CTkEntry(self, placeholder_text="Doctor name")
-        self.doctor_entry.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
+        self.doctor_entry.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
+        self.doctor_label = CTK.CTkLabel(self, text="Doctor's name : ")
+        self.doctor_label.grid(row=2, column=0, padx=5, pady=5)
+
         self.hour_entry = CTK.CTkEntry(self, placeholder_text="Time of seizure")
-        self.hour_entry.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
+        self.hour_entry.grid(row=3, column=1, padx=5, pady=5, sticky="ew")
+        self.hour_label = CTK.CTkLabel(self, text="Time of seizure : ")
+        self.hour_label.grid(row=3, column=0, padx=5, pady=5)
 
         # Bouton
-        self.boutonOK = CTK.CTkButton(self, text="OK")
+        self.boutonOK = CTK.CTkButton(self, text="OK",fg_color='green', hover_color= ('darkgreen'))
         self.boutonOK.bind("<Button-1>", self.get_metadata)
-        self.boutonOK.grid(row=4, column=0, padx=5, pady=5, sticky="ew")
+        self.boutonOK.grid(row=4, column=1, padx=5, pady=5, sticky="ew")
     
+
     def get_metadata(self, event):
         """
-        recuperer une liste des metadonnées sous la forme [heure réelle, patient, praticien]
+        ecrire une liste des metadonnées sous la forme [heure réelle, patient, praticien]
         """
         
         self.liste.append(self.hour_entry.get())
         self.liste.append(self.doctor_entry.get())
         self.liste.append(self.patient_entry.get())
+        
+        EF.EcrireMetaData(self.liste, self.filename)
 
-        self.closewd = 1
-
-        #return(self.liste)
+        self.destroy()
+        self.update()
         
 
 
