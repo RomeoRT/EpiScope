9f0657cbcfd319a317cb080833e293ff5c9456b1
Romeo <romeo.ramostanghe@gmail.com>
2024-03-12 13:34:51 +0100

    symptom manipulation

---
+++ src/annotation/symptom_label.py
@@ -0,0 +1,21 @@
+# por afficher et gerer les symptomes de la partie droite de l'interface
+
+import customtkinter as CTK
+import class_symptome as symptome
+
+class SymptomLabel :
+    """classe pour gérer l'affichage et la manipulation d'un unique symptome"""
+    def __init__(self, sympt) :
+         """constructeur"""
+
+    
+    
+
+
+
+
+################################################################################################################################
+if __name__=="__main__":
+    root = CTK.CTk()
+
+    root.mainloop()
